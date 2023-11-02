0b4fb095c5e70b3a6e9a67eef9d3065ec5130193
Prashant Trivedi <trivediprashant2000@gmail.com>
2023-11-03 02:41:52 +0530

    --Assigment solve

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,14 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly obj = new RWOnly();
+
+        // Set a value using the setter
+        obj.setName("Rakesh Purohit");
+
+        // Access the value using the getter and print it
+        String retrievedName = obj.getName();
+        System.out.println("Name: " + retrievedName);
+    }
 }
